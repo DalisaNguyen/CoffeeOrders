ccb4f23b33c771856ffdf380bc641a489537560a
Dalisa Nguyen <dalisan@su.fullerton.edu>
2020-10-11 17:45:52 -0700

    initial commit

---
+++ eslintrc.js
@@ -0,0 +1,25 @@
+/* jshint esversion: 6 */
+export default {
+  "env": {
+    "browser": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
